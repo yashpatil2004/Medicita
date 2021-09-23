6fe055344d9093c0250cd9c241e0714d56a22cd4
yashpatil2004 <91257106+yashpatil2004@users.noreply.github.com>
2021-09-23 16:40:03 +0530

    Add files via upload

---
+++ databaseconn.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "medicita";
+
+    $conn = mysqli_connect($servername, $username, $password,$database);
+    if (!$conn) 
+    {
+        echo "Connection Failed";
+        exit;
+    }
+?>
+</body>
+</html>
