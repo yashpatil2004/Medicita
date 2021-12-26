dac74d2292f79230d8b88963e639bd4408ac0838
panchalsumit <91360252+panchalsumit@users.noreply.github.com>
2021-12-26 11:58:39 +0530

    Add files via upload

---
+++ facultyfetch_byid.php
@@ -0,0 +1,16 @@
+<?php
+include "databaseconn.php";
+
+$sql = "select * from faculty where id = '".$_GET['id']."'";
+$exe = mysqli_query($conn,$sql);
+$result = mysqli_fetch_assoc($exe);
+
+if ($result)
+{
+    echo json_encode($result);
+}
+else
+{
+    echo "false";
+}
+?>
